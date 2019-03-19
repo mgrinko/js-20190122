23b6274b0036b8b81a7f4e32b9b055a47385f98f
mgrinko <glukym@gmail.com>
2019-03-19 20:40:27 +0200

    _sendRequest was added

--- js/PhonesService.js
+++ js/PhonesService.js
@@ -1,25 +1,18 @@
 
 
 const PhonesService = {
-  getAll({ onSuccess, onError = () => {} }) {
-    const API_URL = 'https://mgrinko.github.io/js-20190122/api/phones.json';
-    const xhr = new XMLHttpRequest();
-    xhr.open('GET', API_URL, true);
-    xhr.send();
-
-    xhr.onload = () => {
-      if (xhr.status === 200) {
-        onSuccess(JSON.parse(xhr.responseText));
-      } else {
-        onError(`Error occurred: ${xhr.status}: ${xhr.statusText}`);
-      }
-    };
+  getAll({ onSuccess, onError }) {
+    this._sendRequest('/phones', onSuccess, onError);
   },
 
   getById({ phoneId, onSuccess, onError = () => {} }) {
-    const API_URL = `https://mgrinko.github.io/js-20190122/api/phones/${phoneId}.json`;
+    this._sendRequest(`/phones/${phoneId}`, onSuccess, onError);
+  },
+
+  _sendRequest(url, onSuccess, onError = () => {}) {
+    const fullUrl = `https://mgrinko.github.io/js-20190122/api${url}.json`;
     const xhr = new XMLHttpRequest();
-    xhr.open('GET', API_URL, true);
+    xhr.open('GET', fullUrl, true);
     xhr.send();
 
     xhr.onload = () => {
