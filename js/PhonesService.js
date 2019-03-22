02fe806d8c170f88dbaa9ed9a20ebe61c1baa5c3
mgrinko <glukym@gmail.com>
2019-03-22 20:52:25 +0200

    async/await was added

--- js/PhonesService.js
+++ js/PhonesService.js
@@ -12,7 +12,12 @@ const PhonesService = {
       .then(onSuccess, onError)
   },
 
-  _sendRequest(url) {
+  async _sendRequest(url) {
+    const response = await fetch(url);
+    const data = await response.json();
+
+    return data;
+
     return fetch(url)
       .then(response => response.json());
 
