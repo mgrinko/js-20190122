a633e4d9ec2d5099151dd7aab71bef2f4be30f2a
mgrinko <glukym@gmail.com>
2019-03-26 19:39:50 +0200

    eslint was added

--- js/app.js
+++ js/app.js
@@ -1,11 +1,5 @@
 import PhonesPage from './PhonesPage.js';
 
-try {
-  JSON.parse('sdfsdfs  sd fsd f');
-} catch (error) {
-  console.log(error);
-}
-
 const appElement = document.querySelector('[data-component="App"]');
 
 const currentPage = new PhonesPage(appElement);
