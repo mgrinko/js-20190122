0610bb08b83c31d5b6a2fbe051f248aac57e28a5
mgrinko <glukym@gmail.com>
2019-03-26 20:24:13 +0200

    css-loader was added

--- js/app.js
+++ js/app.js
@@ -1,4 +1,7 @@
 import PhonesPage from './PhonesPage.js';
+import text from './app.css';
+
+console.log(text);
 
 const appElement = document.querySelector('[data-component="App"]');
 const currentPage = new PhonesPage(appElement);
